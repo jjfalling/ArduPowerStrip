fb17a723e58210e3a02dbb84af9fbe41772cec7f
jjfalling <jeremy@muginn.local.lan>
2012-09-09 13:16:12 -0700

    Auto commit

--- development/ArduPowerStrip.ino
+++ development/ArduPowerStrip.ino
@@ -43,7 +43,7 @@ const char hostname[] = "APS-rpc1";
 //You need to define the type of relay you are using or how you have it wired.  Also some relays are off when set to low while others are on while set to low. You many need to play with this.
 // To try and make this as simple as possible, lets just use the following settings system-wide:
 // 1 is off=pin low | 0 is off=pin high
-const int relayType = 0;    
+const boolean relayType = 0;    
 
 //What digital pins are your outlets attached to (outlet1 is the first pin listed, outlet2 is the second pin, etc)?
 const int outlets[] = { 
