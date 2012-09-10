74c304ea51ed533306bbeb45dcd38461cf6f36d7
jjfalling <jeremy@muginn.local.lan>
2012-09-10 10:09:31 -0700

    Auto commit

--- development/ArduPowerStrip.ino
+++ development/ArduPowerStrip.ino
@@ -44,10 +44,11 @@ byte subnet[] = {
 //Hostname
 const char hostname[] = "APS-rpc1";
 
-//You need to define the type of relay you are using or how you have it wired.  Also some 
-// relays are off when set to low while others are on while set to low. You many need to play with this.
-// To try and make this as simple as possible, lets just use the following settings system-wide:
-// 1 is off=pin low | 0 is off=pin high
+//You need to define the type of relay you are using or how you have it wired. Some relays
+// are off when set to low while others are on while set to low. You many need to play with this.
+// Trying to simplify this, lets just use the following settings system-wide (assuming your relays
+// are wired for nc)
+//NC: 1 is off=pin low, 0 is off=pin high  | NO: 1 is off=pin high | 0 is off=pin low
 const boolean relayType = 1;    
 
 //What digital pins are your outlets attached to (outlet1 is the first pin listed, outlet2 is the second pin, etc)?
