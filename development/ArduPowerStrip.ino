6a909fbfc6a9ed03f0a828661a01eab5c89b4e54
jjfalling <jeremy@muginn.local.lan>
2012-09-22 14:22:07 -0700

    Auto commit

--- development/ArduPowerStrip.ino
+++ development/ArduPowerStrip.ino
@@ -403,6 +403,7 @@ void setup() {
     unsigned long currentMillis = millis();
     
     if(currentMillis - previousMillisSensor > 2000) {
+    previousMillisSensor = millis();
     
     switch (resetMessage) {
     
